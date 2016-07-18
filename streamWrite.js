079c629413bc4e3783f98a0740135a03099e9925
emre.kiyak <emre.kiyak@surat.com.tr>
2016-07-18 13:58:58 +0300

    last

---
+++ streamWrite.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+var data = 'Simply Easy Learning';
+
+var writeStream = fs.createWriteStream('output.txt');
+writeStream.write(data,'UTF8');
+writeStream.end();
+
+writeStream.on('finish',function () {
+    console.log('write endded');
+});
+
+writeStream.on('error', function(err){
+   console.log(err.stack);
+});
+
+console.log("Program Ended");
